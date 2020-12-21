088b697e0a9cb8c2e8678bc744fdd1407c7dae3c
009kings <009kings@gmail.com>
2020-12-21 14:29:52 -0800

    added some characters

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log('🐸');
-
 // references to DOM Elements I will use.
 // Hot off the press, new way of doing things (let movementDisplay = movement;)
 let movementDisplay = document.getElementById('movement')
@@ -11,13 +9,38 @@ game.height = 400;
 // Get some Context
 let ctx = game.getContext('2d')
 
-// Do sum dwrraring
-// fill color
-ctx.fillStyle = 'white'
-// line color
-ctx.strokeStyle = 'red'
-// line width
-ctx.lineWidth = 20
+// draw a filled box
+let drawBox = (x, y, size, color) => {
+  ctx.fillStyle = color
+  ctx.fillRect(x, y, size, size)
+}
 
-ctx.fillRect(10, 10, 100, 100)
-ctx.strokeRect(0, 0, 100, 100)
+// create some characters
+let ogre = {
+  x: 10,
+  y: 10,
+  color: '#bada55',
+  width: 40,
+  height: 80,
+  alive: true,
+  render: function() {
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.x, this.y, this.width, this.height )
+  }
+}
+
+let hero = {
+  x: 50,
+  y: 50,
+  color: 'hotpink',
+  width: 60,
+  height: 60,
+  alive: true,
+  render: function() {
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.x, this.y, this.width, this.height )
+  }
+}
+
+ogre.x = 100
+ogre.render()
