aa12ec2d8ae27cc1c3e6ff598ce8cf6251698672
009kings <009kings@gmail.com>
2020-12-22 15:12:11 -0800

    constructing characters

--- js/main.js
+++ js/main.js
@@ -9,23 +9,26 @@ let ctx = game.getContext('2d')
 game.setAttribute('height', getComputedStyle(game)['height'])
 game.setAttribute('width', getComputedStyle(game)['width'])
 
-// Ogre
-let ogre = {
-  x: 400,
-  y: 150,
-  color: '#bada55', 
-  width: 60,
-  height: 120,
-  alive: true,
-  render: function() {
+function Crawler(x, y, width, height, color) {
+  this.x = x
+  this.y = y
+  this.color = color
+  this.width = width
+  this.height = height
+  this.alive = true
+  this.render = function() {
     ctx.fillStyle = this.color
     ctx.fillRect(this.x, this.y, this.width, this.height)
   }
 }
 
+let hero = new Crawler(50, 200, 50, 50, 'hotpink')
+let ogre = new Crawler(400, 150, 60, 100, '#bada55')
+
 game.addEventListener('click', (e) => {
   // clear whole board
   ctx.clearRect(0, 0, game.width, game.height)
+  hero.render()
   // change ogre position
   ogre.x = e.offsetX
   ogre.y = e.offsetY
